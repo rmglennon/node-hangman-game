9659ce1c9372f2ab9e758bff077868082abeceee
Rhonda Glennon <rhondaglennon@gmail.com>
2018-02-20 11:58:24 -0800

    add comments and clean up index.js file

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
+// include packages and dependencies; initialize global variables
 var inquirer = require("inquirer");
-
-// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
-// 
-// Randomly selects a word and uses the Word constructor to store it
-// Prompts the user for each guess and keeps track of the user's remaining guesses
-
 var Word = require("./word.js");
-
-// set the number of guesses to the the number of characters in the word and two extras for incorrect letters
 
 var initializePackage = {
   hasRun: false
@@ -15,52 +8,61 @@ var initializePackage = {
 var usedLetters = [];
 var numGuesses;
 
+// function to initialize the game and randomize the list of words. This should only run the very first time.
 function initializeGame() {
   
   console.log("Time to play Hangman! \nFill in all the letters in the word before you run out of guesses! \nThe words are related to produce you can find at a farmer's market.")
   
   initializePackage.hasRun = true;
   
-  
-    var wordList = ["watermelon", "avocado"];
-  // var wordList = ["watermelon", "cauliflower", "pumpkin", "spinach", "lettuce", "celery", "broccoli", "strawberries", "mushrooms", "avocado"];
+  // shuffle the list of words into a new, reordered array
+  var wordList = ["watermelon", "cauliflower", "pumpkin", "spinach", "lettuce", "celery", "broccoli", "strawberries", "mushrooms", "avocado"];
   var wordListReordered = [];
-  
   for (var i = 0; i < 10; i++) {
     var randomIndex = Math.floor(Math.random() * wordList.length);
     var selectedWord = wordList.splice(randomIndex, 1);
     wordListReordered.push(selectedWord.join());
   }
+  
   initializePackage.wordListReordered = wordListReordered;
   return initializePackage;
 }
 
+// function to start the game play and reset variables in subsequent rounds
 function playGame() {
+  
+  // if the game has not been initialized previously, get it set up
   if (!initializePackage.hasRun) {
     initializeGame();
   }
-  // TODO: when wordList is null, end game
+  
+  // grab the first word from the list
   var wordList = initializePackage.wordListReordered.shift();
+  
+  // end the game if there are no words left in the list
   if (wordList.length < 1) {
     console.log("You have guessed all the words. Hope you had a good visit to the farmer's market!");
     return;
   }
+  
+  // make a new word object from the word list 
   var wordToGuess = wordList;
   currentWord = new Word(wordToGuess);
+  
+  // set the number of guesses to the the number of characters in the word and add two extras to consider incorrect letters
   numGuesses = wordToGuess.length + 2;
   usedLetters = [];
-  // console.log("usedLetters " + usedLetters);
+  
+  // start the game and display the word to guess
   console.log("Here is your word: " + "\n" + currentWord.toString());
   getInput(currentWord, wordToGuess);
-
+  
 }
 
-// function to choose a random word from the word list array without repeating any, and make a reordered list of words for each game
-
-
+// function to allow user to input letters with the inquirer package
 function getInput(currentWord, wordToGuess) {
-  // only play if there are guesses left
   
+  // only play if there are guesses left
   if (numGuesses  > 0) {
     
     inquirer.prompt([
@@ -81,29 +83,31 @@ function getInput(currentWord, wordToGuess) {
       }
     ]).then(function(userInput) {
       
-      // don't count letters that are already used
+      // do not count letters that are already used
       if (usedLetters.indexOf(userInput.character) >= 0) {
         console.log("Letter already used. Try again.")
         getInput(currentWord, wordToGuess);
-        //return;
+        
       }
+      
       // continue checking the letters for a match in the word
       else {
-        // userInput returns an object, so need to send the character property to check if the letter is in the word
         
+        // userInput returns an object, so need to send its .character property to check if the letter is in the word
         currentWord.guessLetter(userInput.character);
         
         console.log("Guesses remaining: " + numGuesses);
         
         console.log(currentWord.toString());
         
-        // capture return from function to determine where to put the letter in the array of already guessed letters.. may not need two arrays here
+        // capture return from function to add the letter to the list of already guessed letters.
         usedLetters.push(userInput.character);
-        console.log("used letters " + usedLetters);
+        console.log("Used letters: " + usedLetters);
         
         numGuesses--;
+        
+        // check whether to end the round. If so, then call the reset function; otherwise, use recursion to continue guessing this word.
         var gameState = setGameState(currentWord, numGuesses, wordToGuess);
-
         if (gameState) {
           playGame();
           //return;
@@ -112,28 +116,25 @@ function getInput(currentWord, wordToGuess) {
           getInput(currentWord, wordToGuess);
         }
       }
-      
     });
   }
 }
-//TODO - figure out why the second round has duplicate prompt text
 
+// function to check whether to end the round
 function setGameState(currentWord, numGuesses, wordToGuess) {
-  //check for existence of _ in the word
   
+  //check for existence of _ in the word, which means it is unsolved. If no _ are found, then the round should end.
   if (currentWord.toString().indexOf("_") === -1) {
     console.log("You win!");
-    //  playGame();
     return true;
   } 
-  // needs to be wordToGuess because currentWord is underscores
+  // if there are no guesses left, the round should end. This needs to be wordToGuess because currentWord is underscores.
   if (numGuesses === 0) {
     console.log("You are out of guesses!");
     console.log("The word was " + wordToGuess);
-    //  playGame();
     return true;
   }
 }
 
-// start off by calling playGame() function
+// start off the game by calling playGame() function
 playGame();
